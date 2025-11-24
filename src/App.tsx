b9572cfef10eb8eb8f495957afa0a61e479d6eac
Lewis Moten <lewismoten@gmail.com>
2025-11-24 13:43:49 -0500

    Fetch data from API and cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo } from 'react'
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ff = useQuery({
+    queryKey: ['chartData'],
+    enabled: false,
+    staleTime: Infinity,
+    queryFn: async () => {
+      const res = await fetch('/api/test.php');
+      return res.json();
+    }
+  });
+
+  const getData = () => {
+    if (ff.isFetching) return;
+
+    ff.refetch();
+  }
+
+  const prettyData = useMemo(() => {
+    if (!ff.data) return null;
+    return <pre>{JSON.stringify(ff.data, null, '  ')}</pre>
+  }, [ff.data])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>General Ledger</h1>
+      <button onClick={getData}>{ff.isFetching ? 'Loading...' : 'Get Data'}</button>
+      {ff.error ? <b>{ff.error.message}</b> : null}
+      {prettyData}
+
     </>
   )
 }
