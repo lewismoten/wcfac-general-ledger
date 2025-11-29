04d79282f2d66badeae977aaaddc8477225aef52
Lewis Moten <lewismoten@gmail.com>
2025-11-29 14:49:57 -0500

    import roboto font weights

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import CssBaseline from '@mui/material/CssBaseline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const queryClient = new QueryClient()
 
