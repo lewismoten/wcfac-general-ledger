c506e05843389d11335d0009d06efff7d5dee164
Lewis Moten <lewismoten@gmail.com>
2025-11-30 18:43:09 -0500

    Make Fiscal Year filter auto-complete

--- src/LedgerPage/FyLookup.tsx
+++ src/LedgerPage/FyLookup.tsx
@@ -1,51 +1,77 @@
-import { useCallback, useMemo, type ChangeEvent, type ReactNode } from 'react'
-import { useQuery } from '@tanstack/react-query';
+import { useCallback, useMemo, type ReactNode, type SyntheticEvent } from 'react'
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router-dom';
+import Autocomplete, { type AutocompleteChangeDetails, type AutocompleteChangeReason } from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import { drillDownLevels } from '../utils';
 
 export const FyLookup = ({ name, label }: { label: string, name: string }): ReactNode => {
+
   const [searchParams, setSearchParams] = useSearchParams();
+
   const values = useMemo(() =>
     (searchParams.has(name) ? searchParams.get(name)?.split(',') ?? [] : [])
-      .filter(v => v.trim() !== '')
-    , [name, searchParams]);
+      .filter(v => v.trim() !== ""),
+    [name, searchParams]);
 
-  const { isFetching, error, data } = useQuery({
-    queryKey: ['coa', name],
+  const params = useMemo(() => {
+    const params = new URLSearchParams(searchParams.toString());
+    const idx = drillDownLevels.indexOf(name);
+    if (idx !== -1) {
+      for (let i = idx; i < drillDownLevels.length; i++) {
+        if (params.has(drillDownLevels[i])) {
+          params.delete(drillDownLevels[i]);
+        }
+      }
+    }
+    ['series', 'pg', 'ps'].forEach(key => {
+      if (params.has(key)) params.delete(key);
+    });
+    return params.toString();
+  }, [searchParams]);
+
+  const { data } = useQuery<{ id: string, name: string }[]>({
+    queryKey: ['coa', name, params],
+    placeholderData: keepPreviousData,
     enabled: true,
     queryFn: async () => {
-      const res = await fetch(`/api/lookup-fy.php?type=${name}`);
-      return res.json();
+      const res = await fetch(`/api/lookup-fy.php?type=${name}&${params}`);
+      return res.json().then((data: { id: number, name: string }[]) =>
+        data.map(({ id, name }) => ({
+          id: id.toString(),
+          name
+        })))
     }
   });
 
-  const options = useMemo(() => {
-    if (!data) {
-      if (isFetching) return <option>Fetching</option>;
-      if (error) return <option>Error</option>;
-      return <option>No Data</option>;
-    }
-    return [{ id: "-1", name: "All" }, ...data].map(({ id, name }) => (
-      <option value={id} key={id} selected={values.includes(id.toString())}>{name}</option>
-    ));
-  }, [data, error, isFetching, values])
-
-  const changeSelected = useCallback((event: ChangeEvent<HTMLSelectElement>): void => {
-    let selectedValues = [...event.currentTarget.selectedOptions].map(({ value }) => value);
-    if (selectedValues.length > 1 && selectedValues.includes("-1")) {
-      selectedValues = selectedValues.filter(value => value !== "-1");
-    }
-    if (selectedValues.join(",") !== values.join(",")) {
+  const changeSelected = useCallback((
+    _event: SyntheticEvent<Element, Event>,
+    value: { id: string; name: string; }[],
+    _reason: AutocompleteChangeReason,
+    _details?: AutocompleteChangeDetails<{ id: string; name: string; }> | undefined
+  ): void => {
+    const selectedValues = value.map(v => v.id).sort();
+    if (selectedValues.join(',') !== values.join(',')) {
       if (selectedValues.length === 0) {
         if (!searchParams.has(name)) return;
         searchParams.delete(name);
       } else {
         searchParams.set(name, selectedValues.join(','));
       }
-
       setSearchParams(searchParams);
-    }
-  }, [searchParams, setSearchParams, values]);
+    };
+  }, [setSearchParams, searchParams, values]);
 
-  return <div>{label}<select multiple onChange={changeSelected}>{options}</select></div>;
-
+  return <Autocomplete
+    multiple
+    options={data ?? []}
+    getOptionLabel={(option) => option.name}
+    onChange={changeSelected}
+    value={values.map(id => ({ id, name: data?.find(d => d.id === id)?.name ?? `FY${id.substring(2, 4)}` }))}
+    isOptionEqualToValue={(option, value) => option.id === value.id}
+    renderInput={(params) => (
+      <TextField {...params} label={label} />
+    )}
+    sx={{ width: '500px' }}
+  />
 }
